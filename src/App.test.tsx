f76f935c9bc92134905ebbb6d79ae5db01b39d53
Mathieu Delisle <37332321+jmdtc@users.noreply.github.com>
2021-01-19 12:15:55 +0100

    Update App.test.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,7 +4,7 @@ import App from './App';
 import { Provider } from "react-redux";
 import store from './store/store';
 
-test('renders learn react link', () => {
+test('renders two counters', () => {
   render(<Provider store={store}><App /></Provider>);
   const reactStateCounter = screen.getByText(/React State Counter/i);
   expect(reactStateCounter).toBeInTheDocument();
