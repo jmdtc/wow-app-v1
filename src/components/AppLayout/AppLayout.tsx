cdfcf8efc353da279eb0a08ec4cc685f503a4f78
Mathieu Delisle <mathieudelisle@outlook.com>
2021-02-25 19:45:02 +0100

    added an export file for the sidebar icons

--- src/components/AppLayout/AppLayout.tsx
+++ src/components/AppLayout/AppLayout.tsx
@@ -1,14 +1,16 @@
 import { FC, ReactNode } from "react"
 import { useDispatch } from "react-redux"
 
-import { ReactComponent as Home } from "../../icons/SideBar/home_unfilled.svg"
-import { ReactComponent as HomeFilled } from "../../icons/SideBar/home_filled.svg"
-import { ReactComponent as Rocket } from "../../icons/SideBar/rocket_unfilled.svg"
-import { ReactComponent as RocketFilled } from "../../icons/SideBar/rocket_filled.svg"
-import { ReactComponent as Roster } from "../../icons/SideBar/roster_unfilled.svg"
-import { ReactComponent as RosterFilled } from "../../icons/SideBar/roster_filled.svg"
-import { ReactComponent as Dollar } from "../../icons/SideBar/dollar_unfilled.svg"
-import { ReactComponent as DollarFilled } from "../../icons/SideBar/dollar_filled.svg"
+import {
+    Home,
+    HomeFilled,
+    Rocket,
+    RocketFilled,
+    Roster,
+    RosterFilled,
+    Dollar,
+    DollarFilled,
+} from "../../icons/SideBar/"
 import SideBar from "../SideBar"
 import Main from "../Main"
 
